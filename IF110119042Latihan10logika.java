f08b1ff4b94a466b64de4d43a30d02e4a853573b
rifquabdillah <72897695+rifquabdillah@users.noreply.github.com>
2020-10-20 13:46:12 +0700

    Add files via upload

---
+++ IF110119042Latihan10logika.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanpbo;
+
+/**
+ *
+ * @author Stks
+ */
+public class IF110119042Latihan10logika {
+
+public static void main (String[] args){
+    int umur = 19;
+    boolean dewasa;
+    dewasa = (umur >= 17);
+    System.out.println("Status dewasa : "+  dewasa);
+}    
+}
